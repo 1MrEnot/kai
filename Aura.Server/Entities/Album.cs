b4b80f1283f20c118b4be9a127c937067d2546f7
Максим Еремеев <maxim9236@yandex.ru>
2021-06-03 12:09:07 +0300

    Реализовал добавление альбомов

--- Aura.Server/Entities/Album.cs
+++ Aura.Server/Entities/Album.cs
@@ -16,7 +16,7 @@
 
         public Guid AuthorId { get; set; }
 
-        public string Name { get; set; } = null!;
+        public string Name { get; set; }
 
         public DateTime ReleaseDate { get; set; }
 
